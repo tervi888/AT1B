157ce14654934885c12b9d2d886a9482e0cb7fae
Tervi test <Tervi@list.ru>
2023-07-18 15:19:23 +0300

    one version

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
